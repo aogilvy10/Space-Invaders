5f0956346702ffb9cceba5357ab956f172915cbe
Andrew Ogilvy <ogilvyandrew@gmail.com>
2021-02-19 13:46:07 +0000

    first grid outline but too many lines

---
+++ scripts/app.js
@@ -0,0 +1,60 @@
+function init() {
+
+///VARIABLES 
+const grid = document.querySelector('.grid')
+
+
+
+//DEFINING THE GRID
+const width = 12
+const areaOfDiv = width * width
+const boxes = []
+
+
+
+
+//ALIENS
+// const aliensHit = []
+
+
+//SHOOTER
+const shooterClass = 'shooter'
+const shooterStartPosition = 0
+let currentShooterPosition = 0
+
+//MAKING THE GRID
+
+
+function createGrid(shooterStartPosition) {
+  for( let i = 0; i < areaOfDiv; i++) {
+    const boxNumber = document.createElement('div')
+    boxNumber.innerText = i
+    grid.appendChild(boxNumber)
+    boxes.push(boxNumber)
+  }
+  // addShooter(shooterStartPosition)
+}
+
+
+//ADD SHOOTER
+// function addShooter(position) {
+//   boxes[position].classList.add(shooterClass)
+// }
+
+
+
+
+
+createGrid(shooterStartPosition)
+}
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', init)
